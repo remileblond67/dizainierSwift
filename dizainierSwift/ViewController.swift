1c1693fcb332037a277a14d04a9db3d98f9a7bc0
remileblond67 <leblond.remi@gmail.com>
2015-02-26 09:10:39 +0100

    Finalisation de l'exercice

--- dizainierSwift/ViewController.swift
+++ dizainierSwift/ViewController.swift
@@ -14,10 +14,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var segUnit: UISegmentedControl!
     @IBOutlet weak var segDix: UISegmentedControl!
     @IBOutlet weak var txtValue: UITextField!
-    @IBOutlet weak var btnReset: UIButton!
     @IBOutlet weak var swiGeek: UISwitch!
+    @IBOutlet weak var sldValue: UISlider!
+    @IBOutlet weak var btnReset: UIButton!
     
     var curValue: Int = 0;
+    
+    let magicNumber: Int = 42;
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,7 +33,44 @@ class ViewController: UIViewController {
     }
 
     @IBAction func changeValue(sender: AnyObject) {
-        txtValue.text = String(format: "%2.0f", stpValue.value)
+        
+        if sender === stpValue {
+            println("Ca vient du stepper")
+            curValue = Int(stpValue.value)
+        } else if sender === segUnit {
+            println("Ca vient des unités")
+            println(segUnit.selectedSegmentIndex)
+            curValue = segUnit.selectedSegmentIndex + segDix.selectedSegmentIndex*10
+        } else if sender === segDix {
+            println("Ca vient des dizaines")
+            curValue = segUnit.selectedSegmentIndex + segDix.selectedSegmentIndex*10
+        } else if sender === sldValue {
+            println("Ca vient du slider")
+            curValue = Int(sldValue.value)
+        } else if sender === btnReset {
+            println("Ca vient du reset")
+            curValue = 0
+        }
+        
+        println(curValue)
+        
+        // Affichage de la valeur
+        if swiGeek.on {
+            txtValue.text = String(format: "%x", curValue)
+        } else {
+            txtValue.text = String(format: "%2.0d", curValue)
+        }
+        if curValue == magicNumber {
+            txtValue.textColor = UIColor(red: 1, green: 0, blue: 0, alpha: 1)
+        } else {
+            txtValue.textColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1)
+        }
+        
+        // Mise à jour des contrôleurs
+        stpValue.value = Double(curValue)
+        sldValue.value = Float(curValue)
+        segUnit.selectedSegmentIndex = curValue % 10
+        segDix.selectedSegmentIndex = curValue / 10
     }
     
 
